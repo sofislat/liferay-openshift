eceaedf79e677d67d74c9532a555b2578688c70c
Martin Fabrizzio Vilche <mfvilche@gmail.com>
2019-12-02 18:19:25 -0300

    fix run

--- 7.2/initdb.sh
+++ 7.2/initdb.sh
@@ -16,21 +16,36 @@ fi
 
 
 
-if [ ! -z "$WAITFOR_HOST" ] && [ ! -z "$WAITFOR_PORT" ] ; then
-	echo "···································································································"
-	echo "---->  WAITFOR  ACTIVADO.."
-    until nc -z -v -w5 $WAITFOR_HOST $WAITFOR_PORT &> /dev/null; do echo waiting for $WAITFOR_HOST; sleep 10; done;	
-	echo "···································································································"
-fi
-
 
 echo "INICIANDO LIFERAY...."
 
-if [ -f "/opt/liferay/custom_config/portal-setup-wizard.properties" ]; then
 
+if [ -f "/opt/liferay/custom_config/portal-setup-wizard.properties" ]; then
 cat /opt/liferay/custom_config/portal-setup-wizard.properties > /opt/liferay/portal-setup-wizard.properties
 echo "-----> ARCHIVO CONFIGURACION portal-setup-wizard.properties SETEADO"
-
+else
+echo "-----> ARCHIVO CONFIGURACION portal-setup-wizard.properties NO ENCONTRADO ERROR"
+exit 1
 fi
 
-exec /opt/liferay/tomcat*/bin/catalina.sh run
+
+if [ ! -d "/opt/liferay/data/initdb" ]; then
+
+echo "----> INICIALIZANDO DATABASE"
+/opt/liferay/tomcat*/bin/startup.sh
+
+until curl --max-time 2 http://127.0.0.1:8080 &> /dev/null; do echo waiting for liferay; tail -n 20 /opt/liferay/tomcat*/logs/catalina.out; sleep 10;  done;
+curl --max-time 2 http://127.0.0.1:8080 &> /dev/null
+echo "FINALIZANDO INIT - SALIENDO CON CODIGO "$?
+sleep 10
+mkdir -p /opt/liferay/data/initdb
+kill -15 $(pidof java)
+exit 0
+else 
+echo "-----> LIFERAY DB YA FUE INICIALIZADO"
+fi
+
+
+
+
+
